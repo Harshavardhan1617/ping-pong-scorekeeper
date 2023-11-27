45feeec8ff6cd4423aca595b37e5ba2e9c3b378c
Harshavardhan1617 <harshavardhan1617@gmail.com>
2023-11-27 21:59:48 +0530

    finished adding functionality and basic styling

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ personTwo = 0
 const form = document.querySelector('#scoreform')
 const scoreNum = document.querySelector('#scorenum')
 const maxRounds = document.querySelector('#rounds')
+const scoreOne = document.querySelector('#persononescore')
+const scoreTwo = document.querySelector('#persontwoscore')
+
+
 
 form.addEventListener('submit', function(e){
     e.preventDefault()
@@ -16,16 +20,42 @@ const button1 = document.querySelector('#playerone')
 button1.addEventListener('click', function(){
     if (personOne+personTwo < maxRounds.value){
         personOne+=1
-        scoreNum.innerText = `${personOne} to ${personTwo}`
+        scoreOne.innerText = `${personOne}`
     }
+    colorizer()
+
 })
 
 const button2 = document.querySelector('#playertwo')
 button2.addEventListener('click', function(){
     if (personOne+personTwo < maxRounds.value){
         personTwo+=1
-        scoreNum.innerText = `${personOne} to ${personTwo}`
+        scoreTwo.innerText = `${personTwo}`
     }
+    colorizer()
 
 })
 
+
+function colorizer() {
+    if ((personOne+personTwo == parseInt(maxRounds.value)) && personOne > personTwo){
+        scoreOne.style.color = "green"
+        scoreTwo.style.color = "red"
+    } else if ((personOne+personTwo == parseInt(maxRounds.value)) && personOne < personTwo) {
+        scoreOne.style.color = "red"
+        scoreTwo.style.color = "green"
+    }
+}
+
+const reset = document.querySelector('#reset')
+
+reset.addEventListener('click', function(){
+    personOne = 0
+    personTwo = 0
+    scoreOne.innerText = `${personOne}`
+    scoreTwo.innerText = `${personTwo}`
+    scoreOne.style.color = "rgb(0 0 0)"
+    scoreTwo.style.color = "rgb(0 0 0)"
+    maxRounds.value = ""
+    
+})
